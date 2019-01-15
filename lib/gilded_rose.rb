505cfa7f710ea8681b0f92b8aef0c27ba93c1bc1
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 14:18:08 +0000

    special_increasin_ticket refactored

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -23,16 +23,15 @@ class GildedRose
   end
 
   def special_increasing_ticket(item)
-    if item.quality <= 50
-      if item.sell_in <= 0
-        item.quality = 0
-      elsif item.sell_in <= 10 && item.sell_in > 5
-        item.quality < 49 ? item.quality += 2 : item.quality += 1
-      elsif item.sell_in <= 5 && item.sell_in > 0
-        item.quality < 48 ? item.quality += 3 : item.quality += 2
-      elsif item.quality < 50
-        item.quality += 1
-      end
+    return unless item.quality <= 50
+    if item.sell_in <= 0
+      item.quality = 0
+    elsif item.sell_in <= 10 && item.sell_in > 5
+      item.quality < 49 ? item.quality += 2 : item.quality += 1
+    elsif item.sell_in <= 5 && item.sell_in > 0
+      item.quality < 48 ? item.quality += 3 : item.quality += 2
+    elsif item.quality < 50
+      item.quality += 1
     end
   end
 
