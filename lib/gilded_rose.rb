320da7a4e47ee45557d0f6752323bd9bdbfcae46
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 13:38:19 +0000

    increasing ticket refactored

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,23 +9,16 @@ class GildedRose
 
   def generic_ticket(item)
     if item.quality > 0
-      item.sell_in > 0 ? item.quality -= 1 : item.quality -=2
+      item.sell_in > 0 ? item.quality -= 1 : item.quality -= 2
     end
     item.sell_in -= 1
   end
 
   def increasing_ticket(item)
-    if item.sell_in  > 0
-      if item.quality < 50
-        item.quality += 1
-      end
-    end
-    if item.sell_in < 1
-      if item.quality < 49
-        item.quality += 2
-      else
-        item.quality += 1
-      end
+    if item.sell_in > 0 && item.quality < 50
+      item.quality += 1
+    elsif item.sell_in < 1
+      item.quality < 49 ? item.quality += 2 : item.quality += 1
     end
   end
 
