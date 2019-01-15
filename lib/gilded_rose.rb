148d37af52d6db175cb7a224b8e69acfcebbf328
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-15 14:15:24 +0000

    all tests green having refacotred into 4 helper methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -23,14 +23,14 @@ class GildedRose
   end
 
   def special_increasing_ticket(item)
-    if item.quality < 50
-      if item.sell_in <= 10 && item.sell_in > 5
+    if item.quality <= 50
+      if item.sell_in <= 0
+        item.quality = 0
+      elsif item.sell_in <= 10 && item.sell_in > 5
         item.quality < 49 ? item.quality += 2 : item.quality += 1
       elsif item.sell_in <= 5 && item.sell_in > 0
         item.quality < 48 ? item.quality += 3 : item.quality += 2
-      elsif item.sell_in <= 0
-        item.quality = 0
-      else
+      elsif item.quality < 50
         item.quality += 1
       end
     end
