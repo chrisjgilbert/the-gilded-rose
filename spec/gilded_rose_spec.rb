9fc4bbbcebf001010d93cc2651478463ad3ad042
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-16 13:42:33 +0000

    all sulfuras trsts now green

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -39,18 +39,6 @@ describe GildedRose do
     end
 
     context 'when item is Sulfuras, Hand of Ragnaros' do
-      it 'does not change sell_in' do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
-        GildedRose.new([item]).update_quality
-        expect(item.sell_in).to eq(1)
-      end
-
-      it 'does not change quality' do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
-        GildedRose.new([item]).update_quality
-        expect(item.quality).to eq(80)
-      end
-
       it 'tells sulfuras to update' do
         item = double(:item, name: "Sulfuras, Hand of Rangaros")
         expect(item).to receive(:update)
