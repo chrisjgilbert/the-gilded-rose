0a36047b6c345ac7737d0d1b452279248ca813df
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-16 13:14:15 +0000

    set gilded rose generic tests to pending

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,13 +10,13 @@ describe GildedRose do
       expect(item.name).to eq "foo"
     end
 
-    it "lowers the sell_in by one after a day" do
+    xit "lowers the sell_in by one after a day" do
       item = Item.new("item", 1, 0)
       GildedRose.new([item]).update_quality
       expect(item.sell_in).to eq 0
     end
 
-    it "never lowers quality below 0" do
+    xit "never lowers quality below 0" do
       item = Item.new("item", 0, 0)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(0)
