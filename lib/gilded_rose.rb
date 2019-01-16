5f2548824db133bc42d0a9e376b261ac7b571dd5
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-16 14:03:53 +0000

    removed big case swtich statement from gilded rose class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,39 +8,10 @@ class GildedRose
   def initialize(items)
     @items = items
   end
-
-  def sulfuras(item)
-    item.update
-  end
-
-  def generic_ticket(item)
-    item.update
-  end
-
-  def aged_brie_ticket(item)
-    item.update
-  end
-
-  def backstage_pass_ticket(item)
-    item.update
-  end
-
+  
   def update_quality
     @items.each do |item|
-      case item.name
-      when "Sulfuras, Hand of Rangaros"
-        sulfuras(item)
-        return
-      when "item"
-        generic_ticket(item)
-        return
-      when "Aged Brie"
-        aged_brie_ticket(item)
-        return
-      when "Backstage passes to a TAFKAL80ETC concert"
-        backstage_pass_ticket(item)
-        return
-      end
+      item.update
     end
   end
 end
